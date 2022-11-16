abbce81d776287a9e4a75b498e1681ba8e668300
lavinashop <89249270+lavinashop@users.noreply.github.com>
2022-11-16 10:49:55 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+<a class="gallery__link" href=${original}>
+  <img
+    class="gallery__image"
+    src=${preview}
+    alt=${description}
+  />
+</a>
+</li>`
+    )
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup());
+// galleryEl.addEventListener("click", onImageClick);
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250 })
+// function onImageClick(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+  
+//   }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+// const iframe = document.querySelector('iframe');
+//     const player = new Vimeo.Player(iframe);
+    import Player from '@vimeo/player';
 
+    const player = new Player('handstick', {
+        id: 19231868,
+        width: 640
+    });
+    
+    player.on('play', function() {
+        console.log('played the video!');
+    });
