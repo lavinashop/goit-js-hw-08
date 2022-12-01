2cf35af3292310ba0e32f930df992dcbc0e8af1e
lavinashop <89249270+lavinashop@users.noreply.github.com>
2022-12-01 17:23:00 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,16 @@
-// const iframe = document.querySelector('iframe');
-//     const player = new Vimeo.Player(iframe);
-    import Player from '@vimeo/player';
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const playerEl = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(playerEl);
+const playerTime = "videoplayer-current-time";
 
-    const player = new Player('handstick', {
-        id: 19231868,
-        width: 640
-    });
     
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    function onTime(data) {
+        localStorage.setItem("videoplayer-current-time", data.seconds)
+    };
+    player.on('timeupdate', throttle(onTime, 1000));
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+ if (localStorage.getItem(playerTime)) {
+    player.setCurrentTime(localStorage.getItem(playerTime))
+ }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const inputForm = document.querySelector('.feedback-form');
+const formFieldKey = 'feedback-form-state';
+const inputValue = JSON.parse(localStorage.getItem(formFieldKey)) || {};
+inputForm.addEventListener('input', throttle(formFields, 500));
+inputForm.addEventListener('submit', formSubmit);
 
+function formFields(evt){
+    if (evt.target.nodeName === "INPUT") {
+        inputValue.email = evt.target.value; 
+    } else if(evt.target.nodeName === "TEXTAREA") {
+        inputValue.message = evt.target.value;  
+    }
+   localStorage.setItem(formFieldKey, JSON.stringify(inputValue))  
+}
+function formSubmit(event){
+event.preventDefault()
+console.log(inputValue);
+event.currentTarget.reset();
+localStorage.removeItem(formFieldKey);
+};
+function updateData(){
+if (localStorage.getItem(formFieldKey)) {
+    const parcedValues = JSON.parse(localStorage.getItem(formFieldKey)|| "")
+    inputForm.elements.email.value = parcedValues.email || "";
+    inputForm.elements.message.value = parcedValues.message || "";
+}
+}
+updateData()
